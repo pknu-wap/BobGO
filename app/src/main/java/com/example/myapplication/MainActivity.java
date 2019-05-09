56844591d6b8eeb407da8ee4a99a1ee998e19f69
yoon_test <yoonda5898@gmail.com>
2019-05-09 22:17:46 +0900

    매칭 목록에 1)사진 2)이름 3)지역 나오게, 입력방식 변경 //수동 입력

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.app.Activity;
 import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -10,14 +11,15 @@ import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Dictionary;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
-    private String[] myDataset={"1","2","3","4","5","6"};
+    private ArrayList<MyData> myDataset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +30,18 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView.setHasFixedSize(true);
 
+        myDataset = new ArrayList<>();
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
         mAdapter = new MyAdapter(myDataset);
         recyclerView.setAdapter(mAdapter);
 
+        myDataset.add(new MyData("최대윤","부산 남구 대연동",R.drawable.backmac));
+        myDataset.add(new MyData("김동현","울산 남구 신정동",R.drawable.mini));
+        myDataset.add(new MyData("박충현","서울 용산구 한남동",R.drawable.toystory));
+        myDataset.add(new MyData("이지영","부산 부산진구 부전동",R.drawable.insideout));
+        myDataset.add(new MyData("나영석","부산 수영구 광안동",R.drawable.backmac));
 
 
 
--- app/src/main/java/com/example/myapplication/MyAdapter.java
+++ app/src/main/java/com/example/myapplication/MyAdapter.java
@@ -12,8 +12,10 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
-    private String[] mDataset;
+    private ArrayList<MyData> mDataset;
 
 
 
@@ -27,18 +29,18 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         public TextView TextView_content;
         public ImageView ImageView_title;
 
-        public MyViewHolder(View v) {
-            super(v);
+        public MyViewHolder(View view) {
+            super(view);
 
-            TextView_title = v.findViewById(R.id.TextView_title);
-            TextView_content = v.findViewById(R.id.TextView_content);
-            ImageView_title = v.findViewById(R.id.ImageView_title);
+            TextView_title = view.findViewById(R.id.TextView_title);
+            TextView_content = view.findViewById(R.id.TextView_content);
+            ImageView_title = view.findViewById(R.id.ImageView_title);
         }
 
     }
 
     // Provide a suitable constructor (depends on the kind of dataset)
-    public MyAdapter(String[] myDataset) {
+    public MyAdapter(ArrayList<MyData> myDataset) {
         mDataset = myDataset;
     }
 
@@ -47,7 +49,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     public MyAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
                                                      int viewType) {
         // create a new view
-        LinearLayout v = (LinearLayout) LayoutInflater.from(parent.getContext())
+        View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.item_cardview, parent, false);
 
         MyViewHolder vh = new MyViewHolder(v);
@@ -59,14 +61,28 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     public void onBindViewHolder(MyViewHolder holder, final int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        holder.TextView_title.setText(mDataset[position]);
+        holder.TextView_title.setText(mDataset.get(position).text);
+        holder.TextView_content.setText(mDataset.get(position).text2);
+        holder.ImageView_title.setImageResource(mDataset.get(position).img);
 
     }
 
     // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
-        return mDataset.length;
+
+        return mDataset.size();
     }
 
+
+
+}
+class MyData{
+    public String text,text2;
+    public int img;
+    public MyData(String text,String text2, int img){
+        this.text=text;
+        this.text2=text2;
+        this.img=img;
+    }
 }
